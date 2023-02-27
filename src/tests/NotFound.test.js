043365e57389921f46f59e8693613560a08c6b7e
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-02-27 20:56:36 -0300

    Testes da pagina NotFound com 100% de cobertura

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Testes NotFound.js', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+
+  it('Verifica se a página contém um heading h2 com o texto Page requested not found', () => {
+    const title = screen.getByRole('heading', { level: 2, name: 'Page requested not found' });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Verifica se a página mostra a imagem correta', () => {
+    const image = screen.getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
